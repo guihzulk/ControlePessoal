f2588286b05832c880cedb0bc94ed54bd539263b
Guilherme Almeida <guilherme_ip1@hotmail.com>
2021-08-24 07:29:23 -0300

    Alterando slider utilizando o Fragment

--- app/src/main/java/com/loja/controlepessoal/MainActivity.java
+++ app/src/main/java/com/loja/controlepessoal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
@@ -17,26 +18,60 @@ public class MainActivity extends IntroActivity {
         setButtonBackVisible(false); //Oculta botão navigation do slider
         setButtonNextVisible(false);
 
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_green_light)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_green_light)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_green_light)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_green_light)
+                .fragment(R.layout.intro_4)
+
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_green_light)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build()
+        );
+
+/*
         addSlide(new SimpleSlide.Builder()
-            .title("Titulo")
-                .description("Descricão")
+            .title("Cadastre-se")
+                .description("Cadastrando você terá acesso a um melhor controle pessoal")
                 .image(R.drawable.um)
                 .background(android.R.color.holo_green_light)
                 .build()
         );
         addSlide(new SimpleSlide.Builder()
-                .title("Titulo 2")
-                .description("Descricão 2")
+                .title("Registre seus gastos")
+                .description("")
                 .image(R.drawable.dois)
                 .background(android.R.color.holo_green_light)
                 .build()
         );
         addSlide(new SimpleSlide.Builder()
-                .title("Titulo 3")
-                .description("Descricão 3")
+                .title("Controle Gastos.")
+                .description("Tenha um melhor controle sobre seus gastos!")
                 .image(R.drawable.tres)
                 .background(android.R.color.holo_green_light)
                 .build()
+
+
         );
+
+ */
     }
 }
